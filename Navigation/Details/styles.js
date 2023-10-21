05184fa77645b0f63da6a79f6fe9762e7533288d
sulayman-ma <suleyman.abba@gmail.com>
2023-10-21 19:28:31 +0100

    Created custom styles & merged to component styles

--- Navigation/Details/styles.js
+++ Navigation/Details/styles.js
@@ -3,8 +3,15 @@ import commonStyles from '../../assets/styles/common-styles';
 
 const styles = StyleSheet.create({
   ...commonStyles,
-  
+
   // ADD COMPONENT SPECIFIC STYLES HERE
+  primaryButton: {
+    marginBottom: 20,
+    borderRadius: 30,
+    color: "#ffffff",
+    padding: 15,
+    backgroundColor: "#1da5cf"
+  }
 });
 
 export default styles
---
+++ assets/styles/common-styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const commonStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#1f2f54',
+      alignItems: 'center',
+      justifyContent: 'center',
+      
+    },
+    textStyle : {
+      fontFamily: "ralewayBold",
+      fontSize: 23,
+      fontWeight: "900",
+      color: "#ffffff"
+    }
+  });
+
+export default commonStyles
